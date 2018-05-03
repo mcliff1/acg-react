d9e92f2c017b554670a25dfe19bca88e291abce9
Matt Cliff <matt@denvercliffs.com>
2018-05-03 13:14:48 -0600

    built in REACT

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
 console.log("Loaded react-dom");
+
+const myWork = [
+  {
+    'title': 'Python',
+    'image': {
+      'desc': 'example screen shot of code',
+      'src': 'images/ex_python1.png',
+      'comment': ''
+    }
+  },
+  {
+    'title': 'Cloud Formation',
+    'image': {
+      'desc': 'example screen shot of json',
+      'src': 'images/ex_cfn1.png',
+      'comment': ''
+    }
+  },
+  {
+    'title': 'Next Word Prediction',
+    'image': {
+      'desc': 'example screen shot of tool',
+      'src': 'images/ex_qknewt.png',
+      'comment': ''
+    }
+  }
+
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
